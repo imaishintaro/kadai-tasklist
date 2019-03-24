095acf5e623aed3ffa95ab3fe31a02b8f2af26e6
shintaro.imai <tetle.ims.prq4@gmail.com>
2019-03-24 03:45:02 +0000

    DRY kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]     
+     
       def index
           @tasks=Task.all
   end
 
   def show
-       @task = Task.find(params[:id])
+     
   end
 
   def new
@@ -24,7 +26,7 @@ class TasksController < ApplicationController
   end
 
    def edit
-    @task = Task.find(params[:id])
+   
   end
 
   def update
@@ -46,6 +48,11 @@ class TasksController < ApplicationController
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
+  end
+  
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
   
     def task_params
